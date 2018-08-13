beddc0ae571277667852be21709a0d098969c2ef
Rebecca Mark <rebecca.lmark@gmail.com>
2018-08-13 16:43:14 -0700

    InvariantList impl for greater simplicity

--- src/main/scala/Functor.scala
+++ src/main/scala/Functor.scala
@@ -6,12 +6,26 @@ trait Functor[F[_]] {
 }
 
 object FunctorInstances {
-  implicit val listFFunctor = {
-    new Functor[ListF] {
-      override def map[A, B](f: A => B): ListF[B, List[B]] = {
-        case NilF => NilF
-        case ConsF(head, tail) => ConsF(head, f(tail)) // Think about this. Why do we call f on the tail.
-      }
-    }
-  }
+//  implicit val listFFunctor = {
+//    new Functor[ListF] {
+//      override def map[A, B](f: A => B): ListF[B, List[B]] = {
+//        case NilF => NilF
+//        case ConsF(head, tail) => ConsF(head, f(tail)) // Think about this. Why do we call f on the tail?
+//      }
+//    }
+//  }
+}
+
+object Functor {
+//  def mapF[A,B,F](f: A => B)(implicit functorInstance: Functor[F]) = {
+//    functorInstance.map(f)
+//  }
+}
+
+object FunctorInstanceRun extends App {
+//  import FunctorInstances._
+//  import Functor._
+//  val testListF = ConsF(1, ConsF(2, NilF))
+//  testListF
+//  mapF(i => i + 1)
 }
--- src/main/scala/ListF.scala
+++ src/main/scala/ListF.scala
@@ -3,17 +3,17 @@ which contains A (the elements in the list)
 and L is a type parameter representing the list
 */
 sealed trait ListF[+A, +L]
-case class NilF() extends ListF[Nothing, Nothing]
+case object NilF extends ListF[Nothing, Nothing]
 case class ConsF[A, L](head: A, tail: L) extends ListF[A,L]
 
 object ListF {
   def in[A]: ListF[A, List[A]] => List[A] = {
-    case _:NilF => Nil
+    case NilF => Nil
     case ConsF(head, tail) => Cons[A](head, tail)
   }
 
   def out[A]: List[A] => ListF[A, List[A]] = {
-    case Nil => NilF()
+    case Nil => NilF
     case Cons(head, tail) => ConsF(head, tail)
   }
 }
